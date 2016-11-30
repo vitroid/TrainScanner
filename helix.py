8bf1bf6db35c82c22956a8ec1a801149b1f2f8a5
vitroid <vitroid@gmail.com>
2016-11-30 16:20:55 +0900

    .

--- helix.py
+++ helix.py
@@ -4,20 +4,26 @@ from __future__ import print_function, division
 import cv2
 import numpy as np
 import math
-
+import argparse
 import sys
 
 #Determine tilt angle by Newton-Raphson method
-#assuming that the aspect ratio is sqrt(2):1
-#The final image size should be px:py = sqrt(2):1
+#assuming that the aspect ratio is a:1
+#The final image size should be px:py = a:1
 #where px = h / sin(theta)
 #and   py = w sin(theta) + h cos(theta)
-def rn_sine(w,h):
+#what we want is t = sin(theta) to satisfy the equations.
+#So,
+#h / t - a * (w * t + h*cos(theta))
+#a t (wt + h cos(theta)) - h == 0 = f(t)
+#a (wtt + h t sqrt(1-tt)) - h == 0 = f(t)
+#f'(t) = a ( 2wt + ht sqrt() + ht .....
+def rn_sine(w,h, aspect=2.0**0.5):
     def f(t):
-        return 2**0.5 *(w*t**2+h*t*(1-t**2)**0.5) - h
+        return aspect *(w*t**2+h*t*(1-t**2)**0.5) - h
     def df(t):
-        return 2**0.5 *(2*w*t+h*(1-t**2)**0.5-h*t**2/(1-t**2)**0.5)
-    t = 0
+        return aspect *(2*w*t+h*(1-t**2)**0.5-h*t**2/(1-t**2)**0.5)
+    t = 0.5
     for i in range(10):
         t = t - f(t)/df(t)
     return t
@@ -25,7 +31,7 @@ def rn_sine(w,h):
 
 
 
-def helicify(img):
+def helicify(img, aspect=2.0**0.5):
     """
     Helicify and project on a A-size paper proportion.
     Note: it fails when the strip is too short.
@@ -34,7 +40,7 @@ def helicify(img):
     #height with a gap
     hg = int(h*1.03)
 
-    sine = rn_sine(w,hg)
+    sine = rn_sine(w,hg,aspect=aspect)
     cosine = (1 - sine**2)**0.5
     px = hg / sine
     py = w*sine + hg*cosine
@@ -77,13 +83,50 @@ def helicify(img):
 
     return canvas2
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("usage: {0} image".format(sys.argv[0]))
-        sys.exit(1)
-    img = cv2.imread(sys.argv[1])
-    canvas2 = helicify(img)
-    cv2.imwrite("{0}.helix.jpg".format(sys.argv[1]), canvas2)
 
+def add_margin(img, margin):
+    h,w = img.shape[:2]
+    if h > w:
+        m = int(h*margin/100)
+    else:
+        m = int(w*margin/100)
+    canvas2 = np.zeros((h+m*2,w+m*2,3), np.uint8)
+    canvas2[:,:,:] = 255
+    canvas2[m:m+h, m:m+w, :] = img[:,:,:]
+    return canvas2
+    
+
+
+def prepare_parser():
+    parser = argparse.ArgumentParser(description='Helicify', fromfile_prefix_chars='@',)
+    parser.add_argument('-m', '--margin', type=float, metavar='x',
+                        default=0,
+                        dest="margin",
+                        help="Add margin of x % of the wider edge around the image.")
+    parser.add_argument('-a', '--aspect', type=float, metavar='x',
+                        default=2.0**0.5,
+                        dest="aspect",
+                        help="Specify the aspect ratio (default=1.414).")
+    parser.add_argument('-o', '--output', type=str, metavar='outfilename',
+                        default="",
+                        dest="output",
+                        help="Output file name.")
+    parser.add_argument('filename', type=str,
+                        help="Image file name.")
+    return parser
+
+
+if __name__ == "__main__":
+    parser = prepare_parser()
+    params = parser.parse_args(sys.argv[1:])
+    print(params.filename)
+    img = cv2.imread(params.filename)
+    canvas2 = helicify(img, aspect=params.aspect)
+    if params.margin != 0:
+        canvas2 = add_margin(canvas2, params.margin)
+    if params.output == "":
+        cv2.imwrite("{0}.helix.jpg".format(params.filename), canvas2)
+    else:
+        cv2.imwrite(params.output, canvas2)
 
 
