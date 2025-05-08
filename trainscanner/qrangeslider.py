aa6fb0552cdb7ebdeb6a5b133060368aaafa6792
vitroid <vitroid@gmail.com>
2025-05-08 18:24:59 +0900

    バージョンを0.14.1に更新し、scikit-videoパッケージを追加しました。また、qrangeslider.pyでのイベント処理において、座標を整数に変換する修正を行いました。

--- trainscanner/qrangeslider.py
+++ trainscanner/qrangeslider.py
@@ -237,9 +237,9 @@ class Handle(RangeSliderElement):
         # When the range is dragged
         event.accept()
         if self.vertical:
-            mx = event.globalPosition().y()
+            mx = int(event.globalPosition().y())
         else:
-            mx = event.globalPosition().x()
+            mx = int(event.globalPosition().x())
         # last value
         _mx = getattr(self, "__mx", None)
 
