e38cb36a82ed5f8eea2d24663e30d8123d394c69
vitroid <vitroid@gmail.com>
2025-05-09 21:38:18 +0900

    Makefileを更新し、Qtのバージョンを6.9.0に変更しました。また、翻訳ファイルのバージョンを2.1に更新し、翻訳内容を最新の状態に保ちました。さらに、ソースファイルのパスを修正しました。

--- trainscanner/i18n/trainscanner_ja.ts
+++ trainscanner/i18n/trainscanner_ja.ts
@@ -1,215 +1,245 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ja_JP">
-<context>
+<TS version="2.1" language="ja_JP">
+  <context>
     <name>EditorGUI</name>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="734"/>
-        <source>1. Seek the first video frame</source>
-        <translation>1. 最初のフレームまで早送りする</translation>
+      <location filename="../trainscanner_gui.py" line="883" />
+      <source>1. Seek the first video frame</source>
+      <translation>1. 最初のフレームまで早送りする</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="769"/>
-        <source>-90</source>
-        <translation>⤷⤷</translation>
+      <location filename="../trainscanner_gui.py" line="916" />
+      <source>-90</source>
+      <translation>⤷⤷</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="772"/>
-        <source>-1</source>
-        <translation>⤷</translation>
+      <location filename="../trainscanner_gui.py" line="919" />
+      <source>-1</source>
+      <translation>⤷</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="775"/>
-        <source>rotation</source>
-        <translation>回転</translation>
+      <location filename="../trainscanner_gui.py" line="922" />
+      <source>rotation</source>
+      <translation>回転</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="901"/>
-        <source>degrees</source>
-        <translation>度</translation>
+      <location filename="../trainscanner_gui.py" line="1056" />
+      <location filename="../trainscanner_gui.py" line="1049" />
+      <location filename="../trainscanner_gui.py" line="1042" />
+      <location filename="../trainscanner_gui.py" line="1035" />
+      <location filename="../trainscanner_gui.py" line="923" />
+      <source>degrees</source>
+      <translation>度</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="778"/>
-        <source>+1</source>
-        <translation>⤶</translation>
+      <location filename="../trainscanner_gui.py" line="925" />
+      <source>+1</source>
+      <translation>⤶</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="781"/>
-        <source>+90</source>
-        <translation>⤶⤶</translation>
+      <location filename="../trainscanner_gui.py" line="928" />
+      <source>+90</source>
+      <translation>⤶⤶</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="825"/>
-        <source>3. Motion Detection and Slit</source>
-        <translation>3. うごきをとらえる/スリットの位置</translation>
+      <location filename="../trainscanner_gui.py" line="979" />
+      <source>3. Motion Detection and Slit</source>
+      <translation>3. うごきをとらえる/スリットの位置</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="842"/>
-        <source>Slit position</source>
-        <translation>スリット位置</translation>
+      <location filename="../trainscanner_gui.py" line="998" />
+      <source>Slit position</source>
+      <translation>スリット位置</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="864"/>
-        <source>2. Repair deformation</source>
-        <translation>2. ゆがみをなおす</translation>
+      <location filename="../trainscanner_gui.py" line="1019" />
+      <source>2. Repair deformation</source>
+      <translation>2. ゆがみをなおす</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SettingsGUI</name>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="209"/>
-        <source>Open a movie</source>
-        <translation>ムービーを開く</translation>
+      <location filename="../trainscanner_gui.py" line="270" />
+      <source>Open a movie</source>
+      <translation>ムービーを開く</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="490"/>
-        <source>(File name appears here)</source>
-        <translation>(ここにファイル名が表示されます)</translation>
+      <location filename="../trainscanner_gui.py" line="592" />
+      <location filename="../trainscanner_gui.py" line="274" />
+      <source>(File name appears here)</source>
+      <translation>(ここにファイル名が表示されます)</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="222"/>
-        <source>Settings</source>
-        <translation>設定</translation>
+      <location filename="../trainscanner_gui.py" line="283" />
+      <source>Settings</source>
+      <translation>設定</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="232"/>
-        <source>Slit mixing</source>
-        <translation>スリットのつなぎ目</translation>
+      <location filename="../trainscanner_gui.py" line="292" />
+      <source>Slit mixing</source>
+      <translation>スリットのつなぎ目</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="237"/>
-        <source>Sharp</source>
-        <translation>くっきり</translation>
+      <location filename="../trainscanner_gui.py" line="301" />
+      <source>Sharp</source>
+      <translation>くっきり</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="244"/>
-        <source>Diffuse</source>
-        <translation>ぼんやり</translation>
+      <location filename="../trainscanner_gui.py" line="309" />
+      <source>Diffuse</source>
+      <translation>ぼんやり</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="253"/>
-        <source>Minimal displacement between the frames</source>
-        <translation>検知できる最小の移動量</translation>
+      <location filename="../trainscanner_gui.py" line="318" />
+      <source>Minimal displacement between the frames</source>
+      <translation>検知できる最小の移動量</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="605"/>
-        <source>pixels</source>
-        <translation>画素</translation>
+      <location filename="../trainscanner_gui.py" line="723" />
+      <location filename="../trainscanner_gui.py" line="535" />
+      <location filename="../trainscanner_gui.py" line="325" />
+      <source>pixels</source>
+      <translation>画素</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="258"/>
-        <source>Small</source>
-        <translation>小</translation>
+      <location filename="../trainscanner_gui.py" line="332" />
+      <source>Small</source>
+      <translation>小</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="266"/>
-        <source>Large</source>
-        <translation>大</translation>
+      <location filename="../trainscanner_gui.py" line="341" />
+      <source>Large</source>
+      <translation>大</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="294"/>
-        <source>Ignore vertical displacements</source>
-        <translation>縦方向のずれを無視する</translation>
+      <location filename="../trainscanner_gui.py" line="350" />
+      <source>Number of frames to estimate the velocity</source>
+      <translation>速度を見積るのに使うコマ数</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="305"/>
-        <source>The train is initially stalling in the motion detection area.</source>
-        <translation>最初、列車は移動検知窓の中に止まっている.</translation>
+      <location filename="../trainscanner_gui.py" line="729" />
+      <location filename="../trainscanner_gui.py" line="713" />
+      <location filename="../trainscanner_gui.py" line="478" />
+      <location filename="../trainscanner_gui.py" line="357" />
+      <source>frames</source>
+      <translation>コマ</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="380"/>
-        <source>Trailing frames</source>
-        <translation>列車が去ったあともスキャンを継続する</translation>
+      <location filename="../trainscanner_gui.py" line="485" />
+      <location filename="../trainscanner_gui.py" line="364" />
+      <source>Short</source>
+      <translation>短</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="610"/>
-        <source>frames</source>
-        <translation>コマ</translation>
+      <location filename="../trainscanner_gui.py" line="494" />
+      <location filename="../trainscanner_gui.py" line="373" />
+      <source>Long</source>
+      <translation>長</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="385"/>
-        <source>Short</source>
-        <translation>短</translation>
+      <location filename="../trainscanner_gui.py" line="381" />
+      <source>Ignore vertical displacements</source>
+      <translation>縦方向のずれを無視する</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="393"/>
-        <source>Long</source>
-        <translation>長</translation>
+      <location filename="../trainscanner_gui.py" line="399" />
+      <source>The train is initially stalling in the motion detection area.</source>
+      <translation>最初、列車は移動検知窓の中に止まっている.</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="414"/>
-        <source>Finish</source>
-        <translation>仕上げ</translation>
+      <location filename="../trainscanner_gui.py" line="417" />
+      <source>Max acceleration</source>
+      <translation>最大加速度</translation>
     </message>
     <message>
-        <location filename="../gui5.py" line="341"/>
-        <source>Stitch to a long image strip</source>
-        <translation type="obsolete">つないで長い画像をつくる</translation>
+      <location filename="../trainscanner_gui.py" line="432" />
+      <source>Tripod</source>
+      <translation>三脚</translation>
     </message>
     <message>
-        <location filename="../ts_conv/converter_gui.py" line="38"/>
-        <source>Add the film perforations</source>
-        <translation>フィルムの縁をつける</translation>
+      <location filename="../trainscanner_gui.py" line="439" />
+      <source>Handheld</source>
+      <translation>手持ち</translation>
     </message>
     <message>
-        <location filename="../ts_conv/converter_gui.py" line="40"/>
-        <source>Make a helical image</source>
-        <translation>らせん画像を作る</translation>
+      <location filename="../trainscanner_gui.py" line="474" />
+      <source>Trailing frames</source>
+      <translation>列車が去ったあともスキャンを継続する</translation>
     </message>
     <message>
-        <location filename="../ts_conv/converter_gui.py" line="44"/>
-        <source>Start</source>
-        <translation>開始</translation>
+      <location filename="../trainscanner_gui.py" line="502" />
+      <source>Debug</source>
+      <translation>デバッグ</translation>
     </message>
     <message>
-        <location filename="../ts_conv/converter_gui.py" line="53"/>
-        <source>Open file</source>
-        <translation>ファイルを開く</translation>
+      <location filename="../trainscanner_gui.py" line="519" />
+      <source>Finish</source>
+      <translation>仕上げ</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="316"/>
-        <source>Max acceleration</source>
-        <translation>最大加速度</translation>
+      <location filename="../trainscanner_gui.py" line="524" />
+      <source>Set the upper bound of the product image width</source>
+      <translation>完成写真の幅の上限を指定</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="330"/>
-        <source>Tripod</source>
-        <translation>三脚</translation>
+      <location filename="../trainscanner_gui.py" line="539" />
+      <source>Start</source>
+      <translation>開始</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="337"/>
-        <source>Handheld</source>
-        <translation>手持ち</translation>
+      <location filename="../trainscanner_gui.py" line="601" />
+      <source>Open file</source>
+      <translation>ファイルを開く</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="274"/>
-        <source>Number of frames to estimate the velocity</source>
-        <translation>速度を見積るのに使うコマ数</translation>
+      <location filename="../trainscanner_gui.py" line="692" />
+      <source>degrees</source>
+      <translation>度</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="418"/>
-        <source>Set the upper bound of the product image width</source>
-        <translation>完成写真の幅の上限を指定</translation>
+      <location filename="../../ts_conv/converter_gui.py" line="53" />
+      <source>Drag &amp; drop an image strip</source>
+      <translation>長い画像をドラッグ&amp;ドロップ</translation>
     </message>
     <message>
-        <location filename="../ts_conv/converter_gui.py" line="35"/>
-        <source>Open an image</source>
-        <translation>画像を開く</translation>
+      <location filename="../../ts_conv/converter_gui.py" line="57" />
+      <source>Add the film perforations</source>
+      <translation>フィルムの縁をつける</translation>
     </message>
     <message>
-        <location filename="../ts_conv/converter_gui.py" line="42"/>
-        <source>Make a rectangular image</source>
-        <translation>長方形画像を作る</translation>
+      <location filename="../../ts_conv/converter_gui.py" line="62" />
+      <source>Do nothing</source>
+      <translation>何もしない</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="400"/>
-        <source>Debug</source>
-        <translation>デバッグ</translation>
+      <location filename="../../ts_conv/converter_gui.py" line="63" />
+      <source>Make a helical image</source>
+      <translation>らせん画像を作る</translation>
     </message>
     <message>
-        <location filename="../trainscanner/trainscanner_gui.py" line="576"/>
-        <source>degrees</source>
-        <translation>度</translation>
+      <location filename="../../ts_conv/converter_gui.py" line="64" />
+      <source>Make a rectangular image</source>
+      <translation>長方形画像を作る</translation>
     </message>
-</context>
+    <message>
+      <location filename="../../ts_conv/converter_gui.py" line="65" />
+      <source>Make a Hans-style image</source>
+      <translation>Hans式のイメージを作成</translation>
+    </message>
+    <message>
+      <location filename="../../ts_conv/converter_gui.py" line="66" />
+      <source>Make a scrolling movie</source>
+      <translation>スクロール動画を生成</translation>
+    </message>
+    <message>
+      <source>Stitch to a long image strip</source>
+      <translation type="vanished">つないで長い画像をつくる</translation>
+    </message>
+    <message>
+      <source>Open an image</source>
+      <translation type="vanished">画像を開く</translation>
+    </message>
+  </context>
 </TS>
