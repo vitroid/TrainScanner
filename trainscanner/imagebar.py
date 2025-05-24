465d37fd56067c88bfa31df79fd5d4d4dd2d05f7
vitroid <vitroid@gmail.com>
2025-05-24 23:23:37 +0900

    画像処理の最適化を行い、OpenCVからQImageへの変換処理を改善しました。また、スリット幅の計算ロジックを修正し、画像の中央を切り出す処理を追加しました。未使用のインポートを削除し、コードの可読性を向上させました。

--- trainscanner/imagebar.py
+++ trainscanner/imagebar.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
+import numpy as np
+import cv2
 
 from PyQt6.QtCore import QPoint
 from PyQt6.QtGui import QImage, QPainter
@@ -9,99 +11,84 @@ from PyQt6.QtWidgets import QApplication, QFrame, QLabel
 
 class ImageBar(QLabel):
     def __init__(self):
-        super(ImageBar, self).__init__()
+        super().__init__()
         self.setFrameStyle(QFrame.Shape.StyledPanel)
         self.thumbs = []
         self.setFixedHeight(100)
-        self.transformer = lambda x,: x  # no conversion
+        self.transformer = lambda x: x  # no conversion
         self.minwidth = 5  # Minimum width of a slit
-        # self._prepareImage()
 
     def paintEvent(self, event):
-        self._prepareImage()
+        if not self.thumbs:
+            return
+
+        painter = QPainter(self)
+        destwidth = self.width()
+        division = len(self.thumbs)
+        slit_width = max(self.minwidth, destwidth // division + 1)
+
+        # スリット幅が最小幅より小さい場合、分割数を調整
+        if slit_width == self.minwidth:
+            division = destwidth // slit_width - 1
+
+        for slit in range(division):
+            point = QPoint(slit * destwidth // division, 0)
+            thumb = self.transformer(self.thumbs[slit * len(self.thumbs) // division])
+
+            if thumb.width() > slit_width:
+                # 画像がスリット幅より大きい場合は中央を切り出し
+                w0 = (thumb.width() - slit_width) // 2
+                thumb = thumb.copy(w0, 0, slit_width, thumb.height())
+
+            painter.drawImage(point, thumb)
 
     def setTransformer(self, func):
         self.transformer = func
 
-    def _prepareImage(self):
-        """
-        set the pixmap here.
-        """
-        painter = QPainter(self)
-        if len(self.thumbs) == 0:
-            return
-        destwidth = self.width()
-        division = len(self.thumbs)
-        NF = division
-        slit_width = destwidth // division + 1
-        if slit_width < self.minwidth:
-            slit_width = self.minwidth
-            division = destwidth // slit_width - 1
-        for slit in range(division):
-            point = QPoint(slit * destwidth // division, 0)
-            i = slit * NF // division
-            thumb = self.transformer(self.thumbs[i])
-            w = thumb.width()
-            h = thumb.height()
-            if w > slit_width:
-                w0 = (w - slit_width) // 2
-                cropped = thumb.copy(w0, 0, slit_width, h)
-                painter.drawImage(point, cropped)
-            else:
-                painter.drawImage(point, thumb)
-
     def setThumbs(self, thumbs):
         self.thumbs = thumbs
-        self.repaint()  # repaint() will trigger the paintEvent(self, event), this way the new pixmap will be drawn on the label
+        self.repaint()
 
 
 def cv2toQImage(cv2image):
-    """
-    It breaks the original image
-    """
-    import numpy as np
-
-    height, width = cv2image.shape[0:2]
-    tmp = cv2image[:, :, 0].copy()
-    cv2image[:, :, 0] = cv2image[:, :, 2]
-    cv2image[:, :, 2] = tmp
-    return QImage(cv2image.data, width, height, width * 3, QImage.Format.Format_RGB888)
+    """Convert OpenCV image to QImage"""
+    height, width = cv2image.shape[:2]
+    # BGR to RGB conversion
+    rgb_image = cv2.cvtColor(cv2image, cv2.COLOR_BGR2RGB)
+    return QImage(rgb_image.data, width, height, width * 3, QImage.Format.Format_RGB888)
 
 
 def main():
-    import sys
-
     app = QApplication(sys.argv)
     window = ImageBar()
     window.resize(300, 100)
     window.show()
 
-    # from trainscanner import video
-    import cv2
     import trainscanner.video as video
 
     vl = video.VideoLoader("examples/sample2.mov")
-    ret = True
     thumbs = []
+
     while True:
         nframe, frame = vl.next()
         if nframe == 0:
             break
-        h, w = frame.shape[0:2]
+
+        # サムネイルの作成
         thumbh = 100
-        thumbw = w * thumbh // h
+        thumbw = frame.shape[1] * thumbh // frame.shape[0]
         thumb = cv2.resize(frame, (thumbw, thumbh), interpolation=cv2.INTER_CUBIC)
         thumbs.append(cv2toQImage(thumb))
-        terminate = False
-        for i in range(9):
-            nframe = vl.skip()
-            if nframe == 0:
-                terminate = True
-                break
-        if terminate:
-            break
-    window.setThumbs(thumbs)
 
+        # 9フレームスキップ
+        for _ in range(9):
+            if vl.skip() == 0:
+                break
+        else:
+            continue
+        break
+
+    window.setThumbs(thumbs)
     sys.exit(app.exec())
 
 
