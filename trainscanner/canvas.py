3d86f7b5ecccb1bd81f8168b3ef8a0fe9a5f1015
vitroid <vitroid@gmail.com>
2025-05-24 23:23:15 +0900

    キャンバスの境界計算を改善し、画像追加時のキャンバスサイズ変更処理を最適化しました。未使用のフラグを削除し、コードの可読性を向上させました。

--- trainscanner/canvas.py
+++ trainscanner/canvas.py
@@ -8,7 +8,6 @@ class Canvas:
     def __init__(self, image=None, position=None):
         self._image = image
         self.origin = position
-        self.first = True
 
     def done(self):
         """
@@ -24,41 +23,55 @@ class Canvas:
             self._image = add_image.copy()
             self.origin = pos
             return
-        if self.first:
-            self.first = False
+
         x, y = pos
         absx, absy = self.origin  # absolute coordinate of the top left of the canvas
-        cxmin = absx
-        cymin = absy
-        cxmax = self._image.shape[1] + absx
-        cymax = self._image.shape[0] + absy
-        ixmin = x
-        iymin = y
-        ixmax = add_image.shape[1] + x
-        iymax = add_image.shape[0] + y
 
-        xmin = min(cxmin, ixmin)
-        xmax = max(cxmax, ixmax)
-        ymin = min(cymin, iymin)
-        ymax = max(cymax, iymax)
+        # キャンバスと追加画像の境界を計算
+        canvas_bounds = (
+            absx,
+            absy,
+            self._image.shape[1] + absx,
+            self._image.shape[0] + absy,
+        )
+        image_bounds = (x, y, add_image.shape[1] + x, add_image.shape[0] + y)
+
+        # 新しいキャンバスの境界を計算
+        xmin = min(canvas_bounds[0], image_bounds[0])
+        xmax = max(canvas_bounds[2], image_bounds[2])
+        ymin = min(canvas_bounds[1], image_bounds[1])
+        ymax = max(canvas_bounds[3], image_bounds[3])
+
+        # キャンバスのサイズが変更必要な場合のみ新しいキャンバスを作成
         if (xmax - xmin, ymax - ymin) != (self._image.shape[1], self._image.shape[0]):
             newcanvas = np.zeros((ymax - ymin, xmax - xmin, 3), np.uint8)
-            newcanvas[cymin - ymin : cymax - ymin, cxmin - xmin : cxmax - xmin, :] = (
-                self._image[:, :, :]
-            )
+            newcanvas[
+                canvas_bounds[1] - ymin : canvas_bounds[3] - ymin,
+                canvas_bounds[0] - xmin : canvas_bounds[2] - xmin,
+                :,
+            ] = self._image
         else:
             newcanvas = self._image
+
+        # 画像を追加
         if linear_alpha is None:
-            newcanvas[iymin - ymin : iymax - ymin, ixmin - xmin : ixmax - xmin, :] = (
-                add_image[:, :, :]
-            )
+            newcanvas[
+                image_bounds[1] - ymin : image_bounds[3] - ymin,
+                image_bounds[0] - xmin : image_bounds[2] - xmin,
+                :,
+            ] = add_image
         else:
             newcanvas[
-                iymin - ymin : iymax - ymin, ixmin - xmin : ixmax - xmin, :
-            ] = add_image[:, :, :] * linear_alpha[:, :] + newcanvas[
-                iymin - ymin : iymax - ymin, ixmin - xmin : ixmax - xmin, :
+                image_bounds[1] - ymin : image_bounds[3] - ymin,
+                image_bounds[0] - xmin : image_bounds[2] - xmin,
+                :,
+            ] = add_image * linear_alpha[:, :] + newcanvas[
+                image_bounds[1] - ymin : image_bounds[3] - ymin,
+                image_bounds[0] - xmin : image_bounds[2] - xmin,
+                :,
             ] * (
                 1 - linear_alpha[:, :]
             )
+
         self._image = newcanvas
         self.origin = (xmin, ymin)
