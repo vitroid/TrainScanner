1f060b702e201c0bb504791c28525984f71e4f71
vitroid <vitroid@gmail.com>
2025-06-05 03:11:21 +0900

    VideoLoaderクラスのnextメソッドを修正し、フレームの取得方法をディレクトリ内のPNGファイルのソートされたリストから取得するように変更しました。これにより、ファイル名がソート可能であれば自由に命名できるようになりました。

--- trainscanner/video/image_dir.py
+++ trainscanner/video/image_dir.py
@@ -12,9 +12,17 @@ class VideoLoader(object):
     def __init__(self, filename):
         self.dirname = filename
         self.nframe = 0
+        # ディレクトリ内のファイルをソートしておく
+        self.filenames = sorted(
+            [
+                f"{self.dirname}/{f}"
+                for f in os.listdir(self.dirname)
+                if f.endswith(".png")
+            ]
+        )
 
     def next(self):
-        filename = f"{self.dirname}/{self.nframe:06d}.png"
+        filename = self.filenames[self.nframe]
         frame = cv2.imread(filename)
         self.nframe += 1
         if frame is None:
